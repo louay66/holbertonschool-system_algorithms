7e6252153cf2e9782444bbf3839e2111d4693367
louay66 <louaylouay20@gmail.com>
2023-04-03 02:31:05 +0100

    0-graph_create.c

--- 0x01-graphs/0-graph_create.c
+++ 0x01-graphs/0-graph_create.c
@@ -4,23 +4,22 @@
 #include "graphs.h"
 
 /**
- * graph_create - a function that allocates memory
- * to store a graph_t structure,
- * and initializes its content.
- * Return: A pointer to the allocated structure, or NULL on failure.
+ * graph_create - create new graph
+ *
+ * Return: pointer to new graph or NULL if error
  */
-
 graph_t *graph_create(void)
 {
-	graph_t *new = NULL;
+	graph_t *new_graph = NULL;
 
-	new = malloc(sizeof(graph_t));
-
-	if (new == NULL)
+	new_graph = malloc(sizeof(graph_t));
+	if (!new_graph)
+	{
 		return (NULL);
+	}
+	/*--------init graph----------*/
+	new_graph->nb_vertices = 0;
+	new_graph->vertices = NULL;
 
-	new->nb_vertices = 0;
-	new->vertices = NULL;
-
-	return (new);
+	return (new_graph);
 }
