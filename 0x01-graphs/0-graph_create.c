bc4f7015d87458e14ccb1bbc76d5c13620781428
louay66 <louaylouay20@gmail.com>
2023-04-03 02:20:08 +0100

    0-graph_create.c

--- 0x01-graphs/0-graph_create.c
+++ 0x01-graphs/0-graph_create.c
@@ -10,41 +10,16 @@
  */
 graph_t *graph_create(void)
 {
-	vertex_t *new_vertex;
-	edge_t *new_edge;
-	graph_t *new_graph;
+	graph_t *new_graph = NULL;
 
-	new_edge = malloc(sizeof(edge_t));
-	if (!new_edge)
-	{
-		return (NULL);
-	}
-	new_vertex = malloc(sizeof(vertex_t));
-	if (!new_vertex)
-	{
-		free(new_edge);
-		return (NULL);
-	}
 	new_graph = malloc(sizeof(graph_t));
 	if (!new_graph)
 	{
-		free(new_edge);
-		free(new_vertex);
 		return (NULL);
 	}
-
-	/*--------init edges----------*/
-	new_edge->dest = NULL;
-	new_edge->next = NULL;
-	/*--------init vertex----------*/
-	new_vertex->index = 0;
-	new_vertex->content = NULL;
-	new_vertex->nb_edges = 0;
-	new_vertex->edges = new_edge;
-	new_vertex->next = NULL;
 	/*--------init graph----------*/
 	new_graph->nb_vertices = 0;
-	new_graph->vertices = new_vertex;
+	new_graph->vertices = NULL;
 
 	return (new_graph);
 }
