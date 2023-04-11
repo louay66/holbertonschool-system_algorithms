b68aeb2e5986b40918d98426858e44b7236ba4ed
louay66 <louaylouay20@gmail.com>
2023-04-11 10:28:48 +0100

    3-graph_delete.c

--- 0x01-graphs/3-graph_delete.c
+++ 0x01-graphs/3-graph_delete.c
@@ -3,30 +3,34 @@
 #include <stdio.h>
 
 #include "graphs.h"
-
+/**
+ * graph_delete - delete a graph
+ * @graph: Pointer to the graph
+ * Return: void
+ */
 void graph_delete(graph_t *graph)
 {
-   vertex_t *vertex, *vertex_temp;
-   edge_t *edge, *edge_temp;
+	vertex_t *vertex, *vertex_temp;
+	edge_t *edge, *edge_temp;
 
-   if (!graph)
-      return;
+	if (!graph)
+		return;
 
-   vertex = graph->vertices;
+	vertex = graph->vertices;
 
-   while (vertex)
-   {
-      free(vertex->content);
-      edge = vertex->edges;
-      while (edge)
-      {
-         edge_temp = edge;
-         edge = edge->next;
-         free(edge_temp);
-      }
-      vertex_temp = vertex;
-      vertex = vertex->next;
-      free(vertex_temp);
-   }
-   free(graph);
+	while (vertex)
+	{
+		free(vertex->content);
+		edge = vertex->edges;
+		while (edge)
+		{
+			edge_temp = edge;
+			edge = edge->next;
+			free(edge_temp);
+		}
+		vertex_temp = vertex;
+		vertex = vertex->next;
+		free(vertex_temp);
+	}
+	free(graph);
 }
